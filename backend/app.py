70b784cb222865f54f7bff50d677c2cbf4d1d20c
Abinaya-km <abinayakm0@gmail.com>
2026-03-29 23:20:58 +0530

    remove app.run for production

--- backend/app.py
+++ backend/app.py
@@ -215,9 +215,3 @@ def reset_password():
 
     return jsonify({"status": "success"})
 
-# ---------------- RUN ----------------
-
-port = int(os.environ.get("PORT", 5000))
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=port)
